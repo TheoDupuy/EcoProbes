c9164db38a8dde2ff1e836df6a588972a757d326
clementruffet <37064756+clementruffet@users.noreply.github.com>
2019-02-05 14:16:23 +0100

    Base JS
    Base JS faite

---
+++ Site Web/Js.js
@@ -0,0 +1,331 @@
+<scripts>
+	function alerteSonde1() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		
+		
+		
+		moyenne x derniere valeur;
+		if (valeur > moyenne + y || valeur < moyenne - y)
+			(declarer comme aberant)
+		
+		
+		
+		
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 1 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 1 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 1 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 1 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau de la sonde 1 est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité est de la sonde 1 anormale ");
+				break;
+				
+			return capteur;
+		
+	}
+		function alerteSonde2() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 2 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 2 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 2 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 2 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau de la sonde 2 est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité de la sonde 2 est anormale ");
+				break;
+				
+			return capteur;
+		
+	}
+		function alerteSonde3() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 3 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 3 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 3 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 3 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité est anormale ");
+				break;
+				
+			return capteur;
+		
+	}
+		function alerteSonde4() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 4 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 4 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 4 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 4 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité est anormale ");
+				break;
+				
+			return capteur;
+		
+	}	function alerteSonde5() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 5 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 5 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 5 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 5 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité est anormale ");
+				break;
+				
+			return capteur;
+		
+	}
+		function alerteSonde6() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 6 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 6 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 6 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 6 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité est anormale ");
+				break;
+				
+			return capteur;
+		
+	}
+		function alerteSonde7() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 7 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 7 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 7 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 7 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité est anormale ");
+				break;
+				
+			return capteur;
+		
+	}
+		function alerteSonde8() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 8 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 8 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 8 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 8 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité est anormale ");
+				break;
+				
+			return capteur;
+		
+	}
+		function alerteSonde9() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 9 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 9 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 9 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 9 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité est anormale ");
+				break;
+				
+			return capteur;
+		
+	}
+		function alerteSonde10() {
+		var capteur;
+		var Temperaturesol =/*Valeur de la temperature du sol de la sonde 1*/ ;
+		var Temperatureamb =/*Valeur de la temperature ambiante de la sonde 1*/ ;
+		var Humiditesol =/*Valeur de l'humidité du sol de la sonde 1*/ ;
+		var Humiditeamb =/*Valeur de l'humiditer ambiante de la sonde 1*/ ;
+		var Niveaueau =/*Valeur du niveau de l'eau de la sonde 1*/ ;
+		var Luminositer = /*Valeur de la luminosité de la sonde 1*/ ;
+		switch (capteur) {
+			
+			case 'Temperaturesol':
+				alert(" La température du sol de la sonde 10 est anormale."); 
+				break;
+			case 'Temperatureamb' :
+				alert(" La température de l'air ambiant de la sonde 10 est anormale."); 
+				break;
+			case 'Humiditesol' :
+				alert(" L'humidité du sol de la sonde 10 est anormale."); 
+				break;
+			case 'Humiditeamb':
+				alert(" L'humidité de l'air ambiant de la sonde 10 est anormale."); 
+				break;
+			case 'Niveaueau' :
+				alert(" Le niveau de l'eau est anormale. "); 
+				break;
+			case 'Luminositer' :
+				alert(" La luminosité est anormale ");
+				break;
+				
+			return capteur;
+		
+	}
+</scripts>
